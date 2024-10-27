fc5904b0e2a0b08a27cb22e6d432f91c0a9812d0
mayank93k <mayank93.k@gmail.com>
2024-10-27 22:28:40 +0530

    Updated build.sbt

--- src/main/scala/scala/spark/org/common/constant/ApplicationConstant.scala
+++ src/main/scala/scala/spark/org/common/constant/ApplicationConstant.scala
@@ -1,7 +1,7 @@
 package scala.spark.org.common.constant
 
 object ApplicationConstant {
-  val BaseUrl = "http://200.152.38.155/CNPJ/dados_abertos_cnpj/"
+  val BaseUrl = "" // URL to download file
   val DownloadDirectory = "downloads"
   val FileLinkPattern = """href="([^"]*?\.(zip|csv|txt))""""
   val DatePattern = """href="(\d{4}-\d{2}/)""""
